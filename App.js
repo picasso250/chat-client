b085d0e3d8b97bae5a03bd1d2ec359d9c3627795
picasso250 <cumt.xiaochi@gmail.com>
2018-10-06 20:40:43 +0800

    nav

---
+++ App.js
@@ -0,0 +1,55 @@
+import {
+  createStackNavigator,
+} from 'react-navigation';
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+  Button,
+} from 'react-native';
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigate('Profile', { name: 'Jane' })
+        }
+      />
+    );
+  }
+}
+class ProfileScreen extends React.Component {
+  static navigationOptions = {
+    title: 'ProfileScreen',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigate('Profile', { name: 'Jane' })
+        }
+      />
+    );
+  }
+}
+
+const App = createStackNavigator({
+  Home: { screen: HomeScreen },
+  Profile: { screen: ProfileScreen },
+});
+
+export default App;
